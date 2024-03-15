72df36d25764f950065cdab8ace0d64683d2cd9c
New-Tong <1248449622@qq.com>
2024-03-15 17:11:31 +0800

    peopleinfo的创建

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -3,12 +3,15 @@ from django.db import models
 
 # Create your models here.
 
-class Bookinfo(models.Model):
+class BookInfo(models.Model):
     name = models.CharField(max_length=10, unique=True)
     pub_date = models.DateField(null=True)
     readcount = models.IntegerField(default=0)
     commentcount = models.IntegerField(default=0)
     is_delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'bookinfo'
@@ -16,12 +19,16 @@ class Bookinfo(models.Model):
 
 
 class PeopleInfo(models.Model):
-    name = models.CharField(max_length=10)
-    gender = models.IntegerField()
-    description = models.CharField()
-    is_delete = models.BooleanField()
-    book_id = models.ForeignKey(Bookinfo, True)
+    GENDER_CHOICE = (
+        (1, 'male'),
+        (2, 'female')
+    )
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'peopleinfo'
-        verbose_name = '角色管理'
+        verbose_name = '角色信息'
