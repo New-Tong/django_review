d43f70bb74fbfa100ad182551dc13cf6e805c20a
New-Tong <1248449622@qq.com>
2024-03-15 20:10:50 +0800

    对MySql数据库进行操作

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -5,3 +5,35 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('index')
+
+
+#########################新增数据###########################################
+from book.models import BookInfo
+
+# 方式一
+book = BookInfo(
+    name='Django',
+    pub_date='2000-1-1',
+    readcount=10
+)
+
+# 方式二
+BookInfo.objects.create(name='测试开发入门',
+                        pub_date='2000-1-1',
+                        readcount=100)
+
+BookInfo.objects.get(id=6)
+
+#########################修改数据###########################################
+# 方式一
+book = BookInfo.objects.get(id=6)
+book.name = '运维入门'
+book.save()
+# 方式二
+BookInfo.objects.filter(id=5).update(name='Flask', commentcount=666)
+
+book.delete()
+
+books = BookInfo.objects.all()
+
+book=BookInfo.objects.filter(id__exact=2)
